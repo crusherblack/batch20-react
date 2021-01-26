8a5dedbe840c7fa9c01224606f887f1774387676
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-01-26 10:22:10 +0700

    handle Event

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -2,19 +2,35 @@ import React,{Component} from 'react';
 import '../App.css'
 
 // class Content extends Component {
+
+//     greeting(){
+//         alert("Hallo Siswa Bootcamp Batch 20")
+//     }
+
 //     render(){
 //       return(
 //         <div className="App-content">
 //           <p>This is Content</p>
+
+//           <button onClick={this.greeting} > 
+//             Hit Me !!!!
+//           </button>
 //         </div>
 //       )
 //     }
 //   }
 
 function Content() {
+
+    function greeting(){
+        alert("hello this is event with Function")
+    }
+
     return(
         <div className="App-content">
             <p>This is Content with Function</p>
+
+            <button onClick={greeting}>Hitt Me !!!</button>
         </div>
     )
 }
